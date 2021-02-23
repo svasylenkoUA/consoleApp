8174e8e3209b396b31e3c913ff646016755a4d6b
Serhii Vasylenko <svasylenko@gloriumtech.com>
2021-02-23 16:04:34 +0200

    minor amendment with input check

--- Program.cs
+++ Program.cs
@@ -13,29 +13,45 @@ namespace ConsoleApp1
                 {
                     Console.WriteLine("Please enter ticket number:");
                     string impData = Console.ReadLine();
-                    if (impData.Length % 2 != 0) {
-                        impData = '0' + impData; 
-                            }
+                    bool isNumeric = int.TryParse(impData, out _);
 
-                    string leftString = impData.Substring(0, impData.Length / 2);
-                    string rightString = impData.Substring(impData.Length / 2);
-
-                    int chL = 0;
-                    int chR = 0;
-
-                    for (int i = 0; i < leftString.Length; i++)
-                    {
-                        chL = chL + leftString[i];
-                        chR = chR + rightString[i];
-                    }
-
-                    if (chL == chR)
+                    if (impData.Length > 3 && impData.Length < 9 && isNumeric)
                     {
 
-                        Console.WriteLine("Lucky one");
+                        if (impData.Length % 2 != 0)
+                        {
+                            impData = '0' + impData;
+                        }
+
+                        string leftString = impData.Substring(0, impData.Length / 2);
+                        string rightString = impData.Substring(impData.Length / 2);
+
+                        int chL = 0;
+                        int chR = 0;
+
+                        for (int i = 0; i < leftString.Length; i++)
+                        {
+                            chL = chL + leftString[i];
+                            chR = chR + rightString[i];
+                        }
+
+                        if (chL == chR)
+                        {
+
+                            Console.WriteLine("Lucky one");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not lucky one");
+                        }
+
+                    } else
+                    {
+                        Console.WriteLine("Number must be 4-8 NUMERIC symbols!");
+
                     }
-                    else { Console.WriteLine("Not lucky one"); 
-                    }
+                
+
                 }
                 catch  (IOException e)
                 {
